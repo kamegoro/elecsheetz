d2e1ae9853c50ba77d5740c43ced86e3faef77ba
kamegoro <tentyo0620@icloud.com>
2023-10-29 13:29:42 +0900

    fix: application files and add tailwind

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -1,13 +1,10 @@
-// Native
 import { join } from 'path';
 import { format } from 'url';
 
-// Packages
 import { BrowserWindow, app, ipcMain, IpcMainEvent, dialog } from 'electron';
 import isDev from 'electron-is-dev';
 import prepareNext from 'electron-next';
 
-// Prepare the renderer once the app is ready
 app.on('ready', async () => {
   await prepareNext('./renderer');
 
@@ -32,10 +29,8 @@ app.on('ready', async () => {
   mainWindow.loadURL(url);
 });
 
-// Quit the app once all windows are closed
 app.on('window-all-closed', app.quit);
 
-// listen the channel `message` and resend the received message to the renderer process
 ipcMain.on('message', (event: IpcMainEvent, message: string) => {
   console.log(message);
   setTimeout(() => event.sender.send('message', 'hi from electron'), 500);
